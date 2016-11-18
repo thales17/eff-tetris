180691f6b53a7b6c60cec58f103d4e4af41d503b
Adam Richardson <ajrich01@gmail.com>
2016-11-17 19:08:07 -0500

    Started adding text to pause menu

--- pause.go
+++ pause.go
@@ -3,11 +3,15 @@ package main
 import "github.com/forestgiant/eff"
 
 type pauseScreen struct {
-	initialized bool
+	letterBlocks []letterBlock
+	initialized  bool
+	progress     float64
+	progressStep float64
 }
 
 func (p *pauseScreen) Init(c eff.Canvas) {
-
+	p.progressStep = float64(1) / float64(25)
+	p.letterBlocks = append(p.letterBlocks, letterBlocksForString("PAUSE", eff.Point{X: 0, Y: 0}, c, false)...)
 	p.initialized = true
 }
 
@@ -23,9 +27,19 @@ func (p *pauseScreen) Draw(c eff.Canvas) {
 		H: c.Height(),
 	}
 
-	c.FillRect(r, eff.Color{R: 0xFF, G: 0xFF, B: 0xFF, A: 0xEE})
+	c.FillRect(r, eff.Color{R: 0xFF, G: 0xFF, B: 0xFF, A: 0xCC})
+
+	for _, block := range p.letterBlocks {
+		block.draw(c)
+	}
 }
 
 func (p *pauseScreen) Update(c eff.Canvas) {
+	if p.progress < 1 {
+		p.progress += p.progressStep
+	}
 
+	for i := range p.letterBlocks {
+		p.letterBlocks[i].rect = p.letterBlocks[i].mover(p.letterBlocks[i].rect, p.progress)
+	}
 }
